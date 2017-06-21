7eee11271c3fcb625adf9eca5638ec986299fd43
Scott <marden.scott@gmail.com>
2017-06-21 12:35:30 -0700

    Initial commit. Building web app to list books/author/category and enable adding books to the list.

---
+++ apps/library_app/models.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+	name = models.CharField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+class Book(models.Model):
+	title = models.CharField(max_length=255)
+	author = models.ForeignKey(Author, related_name = "books")
+	category = models.CharField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
