5bf269e4f2640311004fa25cc89dbf78a7a2c293
Scott <marden.scott@gmail.com>
2017-06-21 13:11:49 -0700

    Finished assignment. Full functionality of listing books in db and adding books to that db. No style at this time.

--- apps/library_app/models.py
+++ apps/library_app/models.py
@@ -4,14 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Author(models.Model):
-	name = models.CharField(max_length=255)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
 
 class Book(models.Model):
 	title = models.CharField(max_length=255)
-	author = models.ForeignKey(Author, related_name = "books")
+	author = models.CharField(max_length=255)
 	category = models.CharField(max_length=255)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.title
--- apps/library_app/views.py
+++ apps/library_app/views.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book
 
 # Create your views here.
 def index(request):
-	return render(request, 'library_app/index.html')
+	books = Book.objects.all()
+	print books
+	context = {
+		'books': books
+	}
+	return render(request, 'library_app/index.html', context)
 
 def add_book(request):
-	return render(request, 'library_app/index.html')
+	if request.method=='POST':
+		Book.objects.create(title=request.POST['title'], author=request.POST['author'], category=request.POST['category'])
+	return redirect('/')
